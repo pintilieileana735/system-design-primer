6a2239e9c3e19978fbebb2780b55985901a0ad51
pintilieileana735 <pintilieileana735@outlook.com>
2025-07-19 11:05:54 +0300

    Create gina.py

---
+++ gina.py
@@ -0,0 +1,17 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True) as sb:
+    url = "https://kick.com/brutalles"
+    sb.uc_open_with_reconnect(url, 5)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
+    sb.uc_gui_handle_captcha()
+    
+    dri2 = sb.get_new_driver(undetectable=True)
+    sb.uc_open_with_reconnect(url, 5)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
+    sb.uc_gui_handle_captcha()
+
+    while(sb.is_element_present('video#video-player')):
+        sb.sleep(10)
